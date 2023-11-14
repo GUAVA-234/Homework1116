8f8934fc12163c4d39411911aa7092ff637189cb
GUAVA-234 <B0910296646@gmail.com>
2023-11-14 21:40:02 +0800

    1114

---
+++ P4~37/P25.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int addbyone (int &xref) ;
+int  main(void)
+{
+int x=100;
+int y=addbyone(x); 
+printf("x=%d\n",x); 
+//system("pause"); 
+}
+int addbyone (int &xref)
+{
+	xref++; 
+	printf("xref= %d\n",xref); 
+	return xref;
+}
+
+
